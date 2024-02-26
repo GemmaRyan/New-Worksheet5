eaa57e7939efb1b827c9f427f73587cdee1a7803
Gemma Ryan <S00250864@atu.ie>
2024-02-26 10:52:19 +0000

    Finishing part 1

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -1,29 +1,58 @@
-﻿namespace Part1
+﻿using System.Threading.Channels;
+
+namespace Part1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Console.Write("Enter sentence: ");
-            //string input = Console.ReadLine();
-            //Console.WriteLine($"{TrimSpaces(input)}");
+            //Q1 Trim Spaces
+            //string input = " a b d s g sg  ";
+            //string triminput = input.Trim();
+            //Console.WriteLine(triminput);
 
-            string secondQ = "asdfrtyghjuik";
+            //Q2 Character positions
+            //int newG = 0;
+            //string positionOfG = "abcdefhijkglmnopqrstuvgwxyz";
+            //newG = positionOfG.IndexOf('g',11);
+            //Console.WriteLine(newG);
 
-        }
-        static string TrimSpaces(string input)
-        {
-            char ans;
-            input.Trim();
-            return input;
-        }
-        static int FirstG(string secondQ)
-        {
-            int ans = 0;
-            ans.IndexOf(secondQ);
+            //Q3 First and Last Characters
+            //string firstAndLast = "abscdefghijk";
+            //string first = firstAndLast.Substring(0,1);
+            //string last = firstAndLast.Substring(firstAndLast.Length-1);
+            //Console.WriteLine(first);
+            //Console.WriteLine(last);
 
-            return ans;
+            //Q4 String Ends nny
+            //string ends = "asghfdjksnny";
+            //bool check;
+            //check = ends.EndsWith("nny");
+            //Console.WriteLine(check);
 
+            //Q5 String Equal
+            //string a = "I love Sligo";
+            //string fake = "I love Sligo";
+            //if (a == fake)
+            //{
+            //    Console.WriteLine("equal");
+            //}
+            //else Console.WriteLine("no");
+
+            //Q6 Split up the words
+            //string a = "ab cd ed gh ij kl";
+            //string [] b = new string [6];
+
+            //b = a.Split();
+            //Console.WriteLine($"{b[0]}\n{b[1]}\n{b[2]}\n{b[3]}\n{b[4]}\n{b[5]}");
+
+            //Q6
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
