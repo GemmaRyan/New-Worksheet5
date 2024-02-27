071203271918b481930f0b4d5c48c8286ba2141c
Gemma Ryan <S00250864@atu.ie>
2024-02-27 18:05:46 +0000

    Part 2 Q1 needs work Def study it and foreach loops and Lists

---
+++ Part 2 Methods/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Part_2_Methods
+{
+    internal class Program
+    {
+        static List<char> abc = new List<char> {'a','b','c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'} ;
+        static List<char> finish = new List<char> { } ;
+        static void Main(string[] args)
+        {
+            string test = "hello";
+
+            Console.WriteLine( CountCharInString("dkfjda;lfjladsfkjl", 'a'));
+            Console.WriteLine(CountCharInString("dkfjda;lfjladsfkjl", 'l'));
+
+           // Occurances(test);
+        }
+
+
+
+        static int[] CountAllCharsInString (string input)
+            {
+            int[] counts = new int[26];
+
+            for (int i = 0; i < abc.Count;  i++) {
+
+                counts[i] = CountCharInString(input, abc[i]);
+            }
+
+            return counts;
+        }
+
+        static int CountCharInString (string input, char c)
+        {
+            int count = 0;
+            for (int i =0; i < input.Length; i++)
+            {
+                if (input[i] == c)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        //static void Occurances(string test)
+        //{
+        //    for (int i = 0; i < test.Length; i++)
+        //    {
+        //        for (int j = 0;j < abc.Count; j++) 
+        //        { 
+        //            if (test[i] == abc[j])
+        //            {
+        //                fi
+        //            }
+        //        }
+        //    }
+        }
+    }
+
--- Part1/Program.cs
+++ Part1/Program.cs
@@ -7,49 +7,50 @@ namespace Part1
         static void Main(string[] args)
         {
             //Q1 Trim Spaces
-            //string input = " a b d s g sg  ";
-            //string triminput = input.Trim();
-            //Console.WriteLine(triminput);
+            string input = " a b d s g sg  ";
+            string triminput = input.Trim();
+            Console.WriteLine(triminput);
 
             //Q2 Character positions
-            //int newG = 0;
-            //string positionOfG = "abcdefhijkglmnopqrstuvgwxyz";
-            //newG = positionOfG.IndexOf('g',11);
-            //Console.WriteLine(newG);
+            int newG = 0;
+            string positionOfG = "abcdefhijkglmnopqrstuvgwxyz";
+            newG = positionOfG.IndexOf('g', 11);
+            Console.WriteLine(newG);
 
             //Q3 First and Last Characters
-            //string firstAndLast = "abscdefghijk";
-            //string first = firstAndLast.Substring(0,1);
-            //string last = firstAndLast.Substring(firstAndLast.Length-1);
-            //Console.WriteLine(first);
-            //Console.WriteLine(last);
+            string firstAndLast = "abscdefghijk";
+            string first = firstAndLast.Substring(0, 1);
+            string last = firstAndLast.Substring(firstAndLast.Length - 1);
+            Console.WriteLine(first);
+            Console.WriteLine(last);
 
             //Q4 String Ends nny
-            //string ends = "asghfdjksnny";
-            //bool check;
-            //check = ends.EndsWith("nny");
-            //Console.WriteLine(check);
+            string ends = "asghfdjksnny";
+            bool check;
+            check = ends.EndsWith("nny");
+            Console.WriteLine(check);
 
             //Q5 String Equal
-            //string a = "I love Sligo";
-            //string fake = "I love Sligo";
-            //if (a == fake)
-            //{
-            //    Console.WriteLine("equal");
-            //}
-            //else Console.WriteLine("no");
+            string n = "I love Sligo";
+            string fake = "I love Sligo";
+            if (n == fake)
+            {
+                Console.WriteLine("equal");
+            }
+            else Console.WriteLine("no");
 
             //Q6 Split up the words
-            //string a = "ab cd ed gh ij kl";
-            //string [] b = new string [6];
+            string a = "ab cd ed gh ij kl";
+            string[] b = new string[6];
 
-            //b = a.Split();
-            //Console.WriteLine($"{b[0]}\n{b[1]}\n{b[2]}\n{b[3]}\n{b[4]}\n{b[5]}");
+            b = a.Split();
+            Console.WriteLine($"{b[0]}\n{b[1]}\n{b[2]}\n{b[3]}\n{b[4]}\n{b[5]}");
 
-            //Q6
-
-
-
+            //Q7 Replace a&bs with t&s
+            string replace = "apples and bananas";
+            string newAs = replace.Replace('a','t');
+            string newLetters = newAs.Replace('b', 's');
+            Console.WriteLine(newLetters);
 
 
             Console.ReadKey();
